21004392120deadda525c77d73baa6a6722a8ec3
Cyrildemand <cydemand@gmail.com>
2024-01-13 21:45:26 +0100

    fix build.sh

--- deploy/linux/build.sh
+++ deploy/linux/build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Nom du conteneur Docker
+nomConteneur="projet-rentree-back"
+
+# Vérifie si un conteneur avec le même nom existe déjà et le supprime le cas échéant
+conteneurExistant=$(docker ps -a -q -f name=^/${nomConteneur}$)
+if [ ! -z "$conteneurExistant" ]; then
+    echo "Un conteneur avec le nom $nomConteneur existe déjà. Suppression du conteneur."
+    docker rm -f $nomConteneur
+fi
+
 # Construit l'image Docker pour le projet Maven
 docker build ../../. -t projet-rentree-back
 echo "Image Docker construite"
-
-# Nom du conteneur Docker
-nomConteneur="projet-rentree-back"
 
 # Lance le conteneur Docker du projet Maven sur le réseau personnalisé, le build est lancé automatiquement
 docker run --name $nomConteneur --network mon-reseau -p 8080:8080 -d projet-rentree-back
--- deploy/powershell/build.ps1
+++ deploy/powershell/build.ps1
@@ -1,14 +1,19 @@
-# Construit l'image Docker pour le projet Maven
-& docker build ../../. -t projet-rentree-back
-
-Write-Host "Image Docker construite"
-
 # Nom du conteneur Docker
 $nomConteneur = "projet-rentree-back"
 
+# Vérifie si un conteneur avec le même nom existe déjà et le supprime le cas échéant
+$conteneurExistant = docker ps -a -q -f "name=^/${nomConteneur}$"
+if ($conteneurExistant) {
+    Write-Host "Un conteneur avec le nom $nomConteneur existe déjà. Suppression du conteneur."
+    docker rm -f $conteneurExistant
+}
+
+# Construit l'image Docker pour le projet Maven
+& docker build ../../. -t projet-rentree-back
+Write-Host "Image Docker construite"
+
 # Lance le conteneur Docker du projet Maven sur le réseau personnalisé, le build est lancé automatiquement
 & docker run --name $nomConteneur --network mon-reseau -p 8080:8080 -d projet-rentree-back
-
 Write-Host "Conteneur Docker lancé"
 
 # Commande à exécuter dans le conteneur
