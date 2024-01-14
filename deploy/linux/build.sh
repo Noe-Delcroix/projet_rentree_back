559f7089fe133a4052610ac9adb43d110a160707
Cyrildemand <cydemand@gmail.com>
2024-01-14 14:13:56 +0100

    test .env

--- deploy/linux/build.sh
+++ deploy/linux/build.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+# Chemin vers le fichier env.env
+env_file="../env.env"
+
+env_value=""
+# Vérifier si le fichier existe
+if [ -f "$env_file" ]; then
+    # Utiliser grep pour rechercher la ligne contenant la variable env
+    env_line=$(grep '^env=' "$env_file")
+
+    # Vérifier si la ligne a été trouvée
+    if [ -n "$env_line" ]; then
+        # Utiliser cut pour extraire la valeur de la variable env
+        env_value=$(echo "$env_line" | cut -d'=' -f2)
+        echo "La valeur de 'env' est : $env_value"
+    else
+        echo "La variable 'env' n'a pas été trouvée dans $env_file."
+    fi
+else
+    echo "Le fichier $env_file n'existe pas."
+fi
 
 # Nom du conteneur Docker
 nomConteneur="projet-rentree-back"
@@ -11,16 +31,16 @@ if [ ! -z "$conteneurExistant" ]; then
 fi
 
 # Construit l'image Docker pour le projet Maven
-docker build ../../. -t projet-rentree-back
+docker build ../../. -t projet-rentree-back:"$env_value"
 echo "Image Docker construite"
 
 # Lance le conteneur Docker du projet Maven sur le réseau personnalisé, le build est lancé automatiquement
-docker run --name $nomConteneur --network mon-reseau -p 8080:8080 -d projet-rentree-back
+docker run --name $nomConteneur --network mon-reseau -p 8080:8080 -d projet-rentree-back-"$env_value"
 echo "Conteneur Docker lancé"
 
 # Commande à exécuter dans le conteneur
 commande="mvn clean install -DskipTests"
 
 # Exécute la commande dans le conteneur Docker
-echo "Exécution de la commande dans le conteneur Docker : $nomConteneur"
+echo "Exécution de la commande dans le conteneur Docker : $nomConteneur $env_value"
 docker exec $nomConteneur $commande
