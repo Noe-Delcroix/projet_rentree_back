6af2a0516b7bd57d9becf3c5f6d331956a574800
Cyrildemand <cydemand@gmail.com>
2024-01-14 14:37:12 +0100

    fix build.sh

--- deploy/linux/build.sh
+++ deploy/linux/build.sh
@@ -39,12 +39,12 @@ docker build ../../. -t projet-rentree-back:"$env_value"
 echo "Image Docker construite"
 
 # Lance le conteneur Docker du projet Maven sur le réseau personnalisé, le build est lancé automatiquement
-docker run --name $nomConteneur-"$env_value" --network mon-reseau -p 8080:8080 -d projet-rentree-back:"$env_value"
+docker run --name $nomConteneur --network mon-reseau -p 8080:8080 -d projet-rentree-back:"$env_value"
 echo "Conteneur Docker lancé"
 
 # Commande à exécuter dans le conteneur
 commande="mvn clean install -DskipTests"
 
 # Exécute la commande dans le conteneur Docker
-echo "Exécution de la commande dans le conteneur Docker : $nomConteneur $env_value"
+echo "Exécution de la commande dans le conteneur Docker : $nomConteneur"
 docker exec $nomConteneur $commande
