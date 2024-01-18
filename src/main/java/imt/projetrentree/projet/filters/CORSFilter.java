79431c9e239b1bac50ea2d0f669b48215a3b06aa
Cyrildemand <cydemand@gmail.com>
2024-01-18 15:07:00 +0100

    fix cors

--- src/main/java/imt/projetrentree/projet/filters/CORSFilter.java
+++ src/main/java/imt/projetrentree/projet/filters/CORSFilter.java
@@ -6,20 +6,27 @@ import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
+
+    private static final List<String> allowedOrigins = Arrays.asList("http://localhost:19006", "http://localhost:19007");
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
 
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:19006");
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:19007");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, token, content-type, accept, authorization");
+        String origin = requestContext.getHeaderString("Origin");
+
+        if (allowedOrigins.contains(origin)) {
+            responseContext.getHeaders().add("Access-Control-Allow-Origin", origin);
+        }
+
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, token, content-type, accept, authorization, x-xsrf-token");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD");
         responseContext.getHeaders().add("Access-Control-Max-Age", "1209600");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, token, content-type, accept, authorization, x-xsrf-token");
     }
 }
